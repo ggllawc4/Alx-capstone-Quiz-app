8a60cd09956adff6e9908efe880894544eb6a108
ggllawc4 <gracelawrence4788@gmail.com>
2024-12-16 06:40:00 +0100

    Moldify html file to include style and deleted previous content in app.jsx

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,12 +1,8 @@
 import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Welcome to the Quiz App</h1>
-      <p className="text-2xl mt-5">Welcome Message</p>
-    </div>
-  );
+  // return (
+  // );
 }
 
 export default App;
